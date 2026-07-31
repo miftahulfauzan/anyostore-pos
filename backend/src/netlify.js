25d4ef1484c41ce06fc514234ddb8ece0ba4882d
anyo <anyo@miftahuls-MacBook-Pro.local>
2026-07-31 17:49:20 +0700

    remove: Netlify deployment setup

--- backend/src/netlify.js
+++
@@ -1,6 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('./app');
-
-exports.handler = serverless(app, {
-  binary: ['image/*', 'video/*', 'application/octet-stream'],
-});
